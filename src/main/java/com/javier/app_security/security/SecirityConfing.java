7a6be8ea1bb58c5ac96374c9aec20e2b865dbac4
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2024-03-23 11:21:20 -0300

    Implementacion del userDetailsService por default

--- src/main/java/com/javier/app_security/security/SecirityConfing.java
+++ src/main/java/com/javier/app_security/security/SecirityConfing.java
@@ -1,10 +1,21 @@
 package com.javier.app_security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecirityConfing {
@@ -16,5 +27,30 @@ public class SecirityConfing {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    /*
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager(){ // Creo dos usuarios con roles diferentes en la memoria implementacion de userDetailService
+        UserDetails admin = User.withUsername("admin")
+                .password("to_be_encoded")
+                .authorities("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("to_be_encoded")
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+    */
+
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean // Bean se pone para que tome la config
+    PasswordEncoder passwordEncoder(){ // Esto es para desarrollar nuestra seguridad, para que ignore la encriptación de la contraseña que exige Spring Security
+        return NoOpPasswordEncoder.getInstance();
     }
 }
